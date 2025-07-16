c45a42e106d40f8a4639b5579381fe91139e0668
Aurgha Das <145524283+aurghadas@users.noreply.github.com>
2025-07-16 10:22:51 +0600

    Delete smoke.py

--- smoke.py
+++
@@ -1,8 +0,0 @@
-from playwright.sync_api import sync_playwright
-
-with sync_playwright() as pw:
-    browser = pw.chromium.launch(headless=True)
-    page = browser.new_page()
-    page.goto("https://example.com")
-    print("Title:", page.title())
-    browser.close()
